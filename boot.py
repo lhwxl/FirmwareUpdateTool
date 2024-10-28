ebc2285c8dcc9c4b194bbec6e010a66d2166c80b
lhwxl <lhw100501@163.com>
2024-10-28 21:28:19 +0800

    更改传输方式

--- boot.py
+++ boot.py
@@ -1,16 +1,28 @@
+import _thread
+import socket
+import network
 from machine import Pin
-from microdot import Microdot
 
 update = Pin(5, Pin.IN, Pin.PULL_UP)
 if update.value() == 0:
-	app = Microdot()
+	wlan = network.WLAN(network.AP_IF)
+	wlan.config(essid="esp32")
+	wlan.active(True)
 
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind(("0.0.0.0", 5000))
+	s.listen(1)
 
-	@app.route("/")
-	def index():
-		return ""
+	data = ""
+	while True:
+		conn, addr = s.accept()
 
+		while True:
+			temp = conn.recv(1024)
+			if len(temp) == 0:
+				conn.close()
+				break
+			data += temp.decode()
 
-	@app.route("/update")
-	def up():
-		return ""
+		# 解析
+
